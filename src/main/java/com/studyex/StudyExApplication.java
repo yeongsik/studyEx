215e89572f2ea5d90e851e96bd08ad9bf522c720
ys <chyoungsik0266@gmail.com>
2023-08-14 16:37:25 +0900

    Initial commit

---
+++ src/main/java/com/studyex/StudyExApplication.java
@@ -0,0 +1,13 @@
+package com.studyex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyExApplication.class, args);
+	}
+
+}
